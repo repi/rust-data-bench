10bebeca6e02acaab23a8d5e3aff066a18d84413
Johan Andersson <repi@repi.se>
2020-01-29 16:53:34 +0100

    Initial commit

---
+++ test-compress/src/main.rs
@@ -0,0 +1,140 @@
+use std::io::Read;
+use std::time::Instant;
+
+struct Codec {
+    pub source: &'static str,
+    pub name: &'static str,
+    pub compress_fn: Box<dyn Fn(&[u8]) -> Vec<u8>>,
+    pub decompress_fn: Box<dyn Fn(&[u8]) -> Vec<u8>>,
+}
+
+fn smush_codecs() -> Vec<Codec> {
+    [
+        ("zstd-0", smush::Codec::Zstd, smush::Quality::Default),
+        ("zstd-1", smush::Codec::Zstd, smush::Quality::Level1),
+        ("zstd-11", smush::Codec::Zstd, smush::Quality::Maximum),
+        ("gzip", smush::Codec::Gzip, smush::Quality::Default),
+        ("deflate", smush::Codec::Deflate, smush::Quality::Default),
+        ("zlib", smush::Codec::Zlib, smush::Quality::Default),
+        ("brotli", smush::Codec::Brotli, smush::Quality::Default),
+        ("lz4-1", smush::Codec::Lz4, smush::Quality::Level1),
+        ("lz4-6", smush::Codec::Lz4, smush::Quality::Default),
+        ("xz", smush::Codec::Xz, smush::Quality::Default),
+    ]
+    .iter()
+    .filter(|(_, codec, _)| smush::is_codec_enabled(*codec))
+    .map(|(name, codec, quality)| Codec {
+        source: "smush",
+        name,
+        compress_fn: Box::new(move |b| smush::encode(b, *codec, *quality).unwrap()),
+        decompress_fn: Box::new(move |b| smush::decode(b, *codec).unwrap()),
+    })
+    .collect::<Vec<_>>()
+}
+
+fn codecs() -> Vec<Codec> {
+    let mut v = smush_codecs();
+
+    v.extend(vec![
+        Codec {
+            source: "lz4-compression",
+            name: "lz4",
+            compress_fn: Box::new(|b| lz4_compression::compress::compress(b)),
+            decompress_fn: Box::new(|b| lz4_compression::decompress::decompress(b).unwrap()),
+        },
+        Codec {
+            source: "snap",
+            name: "snappy",
+            compress_fn: Box::new(|b| snap::Encoder::new().compress_vec(b).unwrap()),
+            decompress_fn: Box::new(|b| snap::Decoder::new().decompress_vec(b).unwrap()),
+        },
+    ]);
+
+    #[cfg(feature = "non_rust")]
+    v.extend(vec![
+        Codec {
+            source: "cloudflare-zlib",
+            name: "zlib-1",
+            compress_fn: Box::new(|b| {
+                let mut deflate =
+                    cloudflare_zlib::Deflate::new(1, cloudflare_zlib::Z_DEFAULT_STRATEGY, 15)
+                        .unwrap();
+                deflate.compress(b).unwrap();
+                deflate.finish().unwrap()
+            }),
+            decompress_fn: Box::new(|b| cloudflare_zlib::inflate(b).unwrap()),
+        },
+        Codec {
+            source: "cloudflare-zlib",
+            name: "zlib-9",
+            compress_fn: Box::new(|b| {
+                let mut deflate =
+                    cloudflare_zlib::Deflate::new(9, cloudflare_zlib::Z_DEFAULT_STRATEGY, 15)
+                        .unwrap();
+                deflate.compress(b).unwrap();
+                deflate.finish().unwrap()
+            }),
+            decompress_fn: Box::new(|b| cloudflare_zlib::inflate(b).unwrap()),
+        },
+    ]);
+
+    #[cfg(feature = "non_rust")]
+    for (comp_name, compression) in &[
+        ("bzip2-fast", bzip2::Compression::Fastest),
+        ("bzip2-best", bzip2::Compression::Best),
+        ("bzip2", bzip2::Compression::Default),
+    ] {
+        v.push(Codec {
+            source: "bzip2",
+            name: comp_name,
+            compress_fn: Box::new(move |b| {
+                let mut out = vec![];
+                bzip2::read::BzEncoder::new(b, *compression)
+                    .read_to_end(&mut out)
+                    .unwrap();
+                out
+            }),
+            decompress_fn: Box::new(|b| {
+                let mut out = vec![];
+                bzip2::read::BzDecoder::new(b)
+                    .read_to_end(&mut out)
+                    .unwrap();
+                out
+            }),
+        });
+    }
+
+    v
+}
+
+fn main() {
+    let datas = vec![
+        ("bincode", include_bytes!("../data/bincode").to_vec()),
+        ("json", include_bytes!("../data/json").to_vec()),
+        ("wasm", include_bytes!("../data/wasm").to_vec()),
+    ];
+
+    for (data_name, data_bytes) in &datas {
+        for codec in &codecs() {
+            let start_time = Instant::now();
+            let compress_bytes = (codec.compress_fn)(&data_bytes);
+            let compress_duration = start_time.elapsed();
+
+            let start_time2 = Instant::now();
+            let decompress_bytes = (codec.decompress_fn)(&compress_bytes);
+            let decompress_duration = start_time2.elapsed();
+
+            assert_eq!(data_bytes, &decompress_bytes);
+
+            println!(
+                "{:20} {:12} {:8} {:.2}x {:>5.0} MB/s {:>5.0} MB/s",
+                codec.source,
+                codec.name,
+                data_name,
+                (data_bytes.len() as f32 / compress_bytes.len() as f32),
+                (data_bytes.len() as f64) / (1024f64 * 1024f64) / compress_duration.as_secs_f64(),
+                (data_bytes.len() as f64) / (1024f64 * 1024f64) / decompress_duration.as_secs_f64(),
+            );
+        }
+    }
+}
---
+++ test-hash/src/main.rs
@@ -0,0 +1,161 @@
+use blake2::Digest as BlakeDigest;
+use std::hash::Hasher;
+use std::time::Instant;
+
+fn u64_to_vec(v: u64) -> Vec<u8> {
+    use byteorder::{LittleEndian, WriteBytesExt};
+
+    let mut vec = vec![];
+    let _ = vec.write_u64::<LittleEndian>(v);
+    vec
+}
+
+#[rustfmt::skip]
+#[allow(clippy::type_complexity)]
+fn hashes() -> Vec<(&'static str, &'static str, Box<dyn Fn(&[u8]) -> Vec<u8>>)> {
+    vec![
+        // twox-hash
+        ( 
+            "twox-hash", "XXHash64", 
+            Box::new(|b| {
+                let mut hasher = twox_hash::XxHash::with_seed(0);
+                hasher.write(&b);
+                u64_to_vec(hasher.finish())
+            }),
+        ),
+
+        // meowhash
+        #[cfg(not(target_arch = "wasm32"))]
+        (
+            "meowhash", "MeowHash",
+            Box::new(|b| {
+                let mut hasher = meowhash::MeowHasher::new();
+                hasher.input(&b);
+                hasher.result().to_vec()
+            }),
+        ),
+        
+        // seahash
+        ( "seahash", "SeaHash", Box::new(|b| u64_to_vec(seahash::hash(b))) ),
+        
+        // t1ha
+        ( "t1ha", "t1ha0", Box::new(|b| u64_to_vec(t1ha::t1ha0(&b, 1))) ),
+        ( "t1ha", "t1ha1", Box::new(|b| u64_to_vec(t1ha::t1ha1(&b, 1))) ),
+
+        // md5
+        ( "md5", "MD5", Box::new(|b| md5::compute(b).to_vec() ) ),
+
+        // sha2
+        ( "sha2", "SHA2-256", Box::new(|b| sha2::Sha256::digest(&b).to_vec()) ),
+        ( "sha2", "SHA2-512", Box::new(|b| sha2::Sha512::digest(&b).to_vec()) ),
+        ( "sha2", "SHA2-512-256", Box::new(|b| sha2::Sha512Trunc256::digest(&b).to_vec()) ),
+        
+        // sha3
+        ( "sha3", "SHA3-256", Box::new(|b| sha3::Sha3_256::digest(&b).to_vec()) ),
+        ( "sha3", "SHA3-512", Box::new(|b| sha3::Sha3_512::digest(&b).to_vec()) ),
+
+
+        // blake2
+        ( "blake2b", "BLAKE2b", Box::new(|b| blake2::Blake2b::digest(&b).to_vec()) ),
+        ( "blake2s", "BLAKE2s", Box::new(|b| blake2::Blake2s::digest(&b).to_vec()) ),
+        ( 
+            "blake2b", "BLAKE2b-256", 
+            Box::new(|b| {
+                use digest::{Input, VariableOutput};
+                let mut hasher = blake2::VarBlake2b::new(32).unwrap();
+                hasher.input(&b);
+                let mut t = vec![];
+                hasher.variable_result(|res| t = res.to_vec());
+                t                
+            }) 
+        ),
+
+
+        // blake2b-simd
+        
+        ( "blake2b-simd", "BLAKE2b", Box::new(|b| blake2b_simd::blake2b(&b).as_bytes().to_vec()) ),
+        ( 
+            "blake2b-simd", "BLAKE2b-256", 
+            Box::new(|b| {
+                let mut params = blake2b_simd::Params::new();
+                params.hash_length(32);
+                params.hash(&b).as_bytes().to_vec()
+            })
+        ),
+        ( "blake2b-simd", "BLAKE2bp", Box::new(|b| blake2b_simd::blake2bp::blake2bp(&b).as_bytes().to_vec()) ),
+        ( 
+            "blake2b-simd", "BLAKE2bp-256", 
+            Box::new(|b| {
+                let mut params = blake2b_simd::blake2bp::Params::new();
+                params.hash_length(32);
+                params.hash(&b).as_bytes().to_vec()
+            })
+        ),
+
+        // blake2s-simd
+
+        ( "blake2s-simd", "BLAKE2s",  Box::new(|b| blake2s_simd::blake2s(&b).as_bytes().to_vec()) ),
+        ( "blake2s-simd", "BLAKE2sp", Box::new(|b| blake2s_simd::blake2sp::blake2sp(&b).as_bytes().to_vec()) ),
+
+        // blake3
+
+        ( "blake3", "BLAKE3",  Box::new(|b| blake3::hash(&b).as_bytes().to_vec()) ),
+
+
+        // multihash
+
+        ( "multihash", "SHA1",       Box::new(|b| multihash::encode(multihash::Hash::SHA1, &b).unwrap().digest().to_vec()) ),
+        ( "multihash", "SHA2-256",   Box::new(|b| multihash::encode(multihash::Hash::SHA2256, &b).unwrap().digest().to_vec()) ),
+        ( "multihash", "SHA2-512",   Box::new(|b| multihash::encode(multihash::Hash::SHA2512, &b).unwrap().digest().to_vec()) ),
+        ( "multihash", "SHA3-256",   Box::new(|b| multihash::encode(multihash::Hash::SHA3256, &b).unwrap().digest().to_vec()) ),
+        ( "multihash", "SHA3-512",   Box::new(|b| multihash::encode(multihash::Hash::SHA3512, &b).unwrap().digest().to_vec()) ),       
+        ( "multihash", "Keccak256",  Box::new(|b| multihash::encode(multihash::Hash::Keccak256, &b).unwrap().digest().to_vec()) ),       
+        ( "multihash", "Keccak512",  Box::new(|b| multihash::encode(multihash::Hash::Keccak512, &b).unwrap().digest().to_vec()) ),       
+        ( "multihash", "BLAKE2b",    Box::new(|b| multihash::encode(multihash::Hash::Blake2b512, &b).unwrap().digest().to_vec()) ),       
+        ( "multihash", "BLAKE2s",    Box::new(|b| multihash::encode(multihash::Hash::Blake2s256, &b).unwrap().digest().to_vec()) ),       
+
+        // ring
+
+        #[cfg(not(target_arch = "wasm32"))]
+        ( "ring", "SHA1", Box::new(|b| ring::digest::digest(&ring::digest::SHA1_FOR_LEGACY_USE_ONLY, b).as_ref().to_vec()) ),
+        #[cfg(not(target_arch = "wasm32"))]
+        ( "ring", "SHA256", Box::new(|b| ring::digest::digest(&ring::digest::SHA256, b).as_ref().to_vec()) ),
+        #[cfg(not(target_arch = "wasm32"))]
+        ( "ring", "SHA512", Box::new(|b| ring::digest::digest(&ring::digest::SHA512, b).as_ref().to_vec()) ),
+        #[cfg(not(target_arch = "wasm32"))]
+        ( "ring", "SHA512-256", Box::new(|b| ring::digest::digest(&ring::digest::SHA512_256, b).as_ref().to_vec()) ),
+
+
+        // sthash
+
+        ( 
+            "sthash", "STHash", 
+            Box::new(|b| {
+                let key = sthash::Key::from_seed(b"this is supposed to be a very long secret key", None);
+                let hasher = sthash::Hasher::new(key, None);
+                hasher.hash(b)
+            })
+        )
+    ]
+}
+
+fn main() {
+    let hashes = hashes();
+
+    let mut bytes = Vec::new();
+    bytes.resize(10 * 1024 * 1024, 0u8);
+
+    for (impl_name, hash_name, hash_func) in &hashes {
+        let start_time = Instant::now();
+
+        let hash_result = hash_func(&bytes);
+
+        println!(
+            "{:13} {:12} {:>5.0} MB/s  {}",
+            impl_name,
+            hash_name,
+            (bytes.len() as f64) / (1024f64 * 1024f64) / start_time.elapsed().as_secs_f64(),
+            multibase::encode(multibase::Base::Base58btc, hash_result)
+        );
+    }
+}
